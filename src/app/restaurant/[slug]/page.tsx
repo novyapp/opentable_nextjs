e3095c6d8f8e3bbe460c02434d7297a9a939fb11
NovyAPP <micnowotnik@gmail.com>
2023-03-08 14:52:00 +0100

    adding layouts

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import NavBar from "@/app/components/NavBar";
-import Link from "next/link";
 import React from "react";
 import Description from "./components/Description";
 import Header from "./components/Header";
@@ -10,28 +8,20 @@ import RestaurantNavbar from "./components/RestaurantNavbar";
 import Reviews from "./components/Reviews";
 import Title from "./components/Title";
 
-type Props = {};
-
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavbar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
+      </div>
+    </>
   );
 }
